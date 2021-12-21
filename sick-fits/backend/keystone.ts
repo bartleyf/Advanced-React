cc344fbd0406407282c837fade1de42569c67d6f
Frank Bartley <bartley.frank@gmail.com>
2021-12-21 14:30:03 -0500

    up to module 4

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,58 @@
+import { createAuth } from '@keystone-next/auth';
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongo://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding
+    },
+    lists: createSchema({
+      // TODO: add schema here
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      // Only show
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+    // TODO Add session values here
+  })
+);
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routerChangeStart', () => NProgress.start());
+Router.events.on('routerChangeComplete', () => NProgress.done());
+Router.events.on('routerChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
